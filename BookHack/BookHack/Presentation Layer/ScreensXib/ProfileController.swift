d13c41f8b2cba67492483be4b45013d881f5d0a3
Sergey <sergey.zaytsev.kaliningrad@gmail.com>
2021-03-20 17:49:11 +0300

    Добавил 2 новых экрана

---
+++ BookHack/BookHack/Presentation Layer/ScreensXib/ProfileController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileController.swift
+//  BookHack
+//
+//  Created by Сергей Зайцев on 20.03.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+}
---
+++ BookHack/BookHack/Presentation Layer/ScreensXib/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  BookHack
+//
+//  Created by Сергей Зайцев on 20.03.2021.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    /// Primary SceneKit view that renders the AR session
+    @IBOutlet var sceneView: ARSCNView!
+    
+    /// A serial queue for thread safety when modifying SceneKit's scene graph.
+    let updateQueue = DispatchQueue(label: "\(Bundle.main.bundleIdentifier!).serialSCNQueue")
+    
+    // MARK: - Lifecycle
+    
+    // Called after the controller's view is loaded into memory.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as FPS and timing information (useful during development)
+        sceneView.showsStatistics = true
+        
+        // Enable environment-based lighting
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    // Notifies the view controller that its view is about to be added to a view hierarchy.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let refImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: Bundle.main) else {
+                fatalError("Missing expected asset catalog resources.")
+        }
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        configuration.trackingImages = refImages
+        configuration.maximumNumberOfTrackedImages = 1
+        
+        // Run the view's session
+        sceneView.session.run(configuration, options: ARSession.RunOptions(arrayLiteral: [.resetTracking, .removeExistingAnchors]))
+    }
+    
+    // Notifies the view controller that its view is about to be removed from a view hierarchy.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
